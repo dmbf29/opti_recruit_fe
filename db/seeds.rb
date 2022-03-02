7d3e93aad37afcc1361cef4ca316c7ce842d80ba
Douglas Berkley <douglasmberkley@gmail.com>
2022-03-02 22:04:50 +0900

    updated values

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+puts "Destroying all players..."
+Player.destroy_all
+
 names = ["Mohamed Salah", "Sadio Mané", "Roberto Firmino", "Trent Alexander-Arnold", "Diogo Jota", "Luis Díaz", "Andrew Robertson", "Virgil van Dijk", " Alisson", "Xherdan Shaqiri", "Alex Oxlade-Chamberlain", " Fabinho", " Thiago", "Harvey Elliott", "Takumi Minamino"]
 
 names.each do |name|
@@ -8,8 +11,9 @@ names.each do |name|
     position: ['FW', 'MF', 'DF', 'GK'].sample,
     overall: overall,
     potential: overall + rand(0..8),
-    value: rand(0..50) + 1000000,
-    wage: rand(1..9) + 10000,
+    value: (rand(1..50).to_s + '000000').to_i,
+    wage: (rand(1..9).to_s + '0000').to_i,
     total: rand(1000..2000)
   )
 end
+puts "... created #{Player.count} players."
