4f30de9302c612f603f6809d0c49a3d0723517d9
Douglas Berkley <douglasmberkley@gmail.com>
2022-03-05 12:03:38 +0900

    updated tabs

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,12 +3,15 @@ class PlayersController < ApplicationController
 
   def index
     # @players = Player.includes(:player_seasons).limit(15)
-    @team = Team.find_by(name: "Liverpool")
+    @team = Team.find_by(id: params[:team_id]) || Team.find_by(name: "Liverpool")
     @players = @team.players.includes(:player_seasons)
   end
 
   def show
     @player = Player.find(params[:id])
-    @players = Player.where.not(id: @player).where.not(position: nil).where.not(team: @player.team).limit(10)
+    @team = @player.team
+    # @players = Player.includes(:player_seasons).where.not(id: @player).where.not(position: nil).where.not(team: @player.team).limit(10)
+    # City.includes(:photos).where(photos: { city_id: nil })
+    @players = Player.includes(:player_seasons).where.not(id: @player).where.not(position: nil).where.not(player_seasons: { team_id: @player.team }).limit(10)
   end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -3,6 +3,7 @@ class Player < ApplicationRecord
   validates :short_name, presence: true
   validates :long_name, presence: true
   validates :sofifa_id, presence: true, uniqueness: true
+  # scope :team, -> { where(published: true) }
 
   def age
     return '-' unless dob
@@ -10,5 +11,9 @@ class Player < ApplicationRecord
     birthday = Date.parse(dob)
     now = Time.now.utc.to_date
     now.year - birthday.year - (birthday.to_date.change(year: now.year) > now ? 1 : 0)
+  end
+
+  def team
+    player_seasons.includes(:season).order("seasons.year desc").last.team
   end
 end
