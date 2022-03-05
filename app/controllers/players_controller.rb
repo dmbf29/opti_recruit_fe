8be37114f10d3f398560a540cd3f063f1ef6e008
Douglas Berkley <douglasmberkley@gmail.com>
2022-03-05 22:47:20 +0900

    player search now working too"

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,5 @@
 class PlayersController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show, :search]
 
   def index
     if params[:team].present?
@@ -15,5 +15,17 @@ class PlayersController < ApplicationController
     @team = @player.team
     @players = @player.search(params).order_unnamed
     @positions = Player.pluck(:position).uniq.sort
+  end
+
+  def search
+    if params[:query].present?
+      @players = Player.where('name ILIKE :query OR long_name ILIKE :query OR short_name ILIKE :query', query: "%#{params[:query]}%").limit(3)
+    else
+      @players = Player.order(:name).limit(3)
+    end
+
+    respond_to do |format|
+      format.text { render partial: 'players/search_card', locals: { players: @players }, formats: [:html] }
+    end
   end
 end
--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -8,11 +8,18 @@ export default class extends Controller {
 
   update() {
     const teamUrl = `/teams/search?query=${this.inputTarget.value}`
-
     fetch(teamUrl, { headers: { "Accept": "text/plain" } })
       .then(response => response.text())
       .then((data) => {
         this.listTarget.innerHTML = data
+      })
+
+    const playerUrl = `/players/search?query=${this.inputTarget.value}`
+    fetch(playerUrl, { headers: { "Accept": "text/plain" } })
+      .then(response => response.text())
+      .then((data) => {
+        this.listTarget.insertAdjacentHTML('beforeend', '<li><hr class="dropdown-divider"></li>')
+        this.listTarget.insertAdjacentHTML('beforeend', data)
       })
   }
 
