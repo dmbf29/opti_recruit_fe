ec4c6b475195b51ca9d059c5290373cf847daf3a
Douglas Berkley <douglasmberkley@gmail.com>
2022-03-10 21:23:33 +0900

    integrated API

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -5,12 +5,13 @@ export default class extends Controller {
   static targets = ['age_min', 'age_max', 'value_min', 'value_max', 'position', 'form', 'table'];
 
   update() {
+    console.log(this.age_minTarget.value)
     const teamUrl = `/players/${this.playerIdValue}?`
     fetch(teamUrl + new URLSearchParams({
-      age_min: this.age_minTarget.value,
-      age_max: this.age_maxTarget.value,
-      value_min: this.value_minTarget.value * 1000000,
-      value_max: this.value_maxTarget.value * 1000000,
+      age_min: this.age_minTarget.value === '' ? 0 : this.age_minTarget.value,
+      age_max: this.age_maxTarget.value === '' ? 99 : this.age_maxTarget.value,
+      value_min: this.value_minTarget.value === '' ? 0 : this.value_minTarget.value * 1000000,
+      value_max: this.value_maxTarget.value === '' ? 999999999 : this.value_maxTarget.value * 1000000,
       position: this.positionTarget.value,
     }), { headers: { "Accept": "text/plain" } })
       .then(response => response.text())
--- app/services/fetch_similar_service.rb
+++ app/services/fetch_similar_service.rb
@@ -2,23 +2,24 @@ class FetchSimilarService
   attr_reader :url, :sofifa_id, :age_min, :age_max, :value_min, :value_max, :position, :params
 
   def initialize(attrs = {})
-    @base_url = ''
+    @base_url = 'https://optirecruitimage-cdv64b7nfq-an.a.run.app'
     @sofifa_id = attrs[:sofifa_id]
     @params = attrs[:params]
-    @age_min = params[:age_min].any? ? params[:age_min] : 0
-    @age_max = params[:age_max].any? ? params[:age_max] : 99
-    @value_min = params[:value_min].any? ? params[:value_min] : 0
-    @value_max = params[:value_max].any? ? params[:value_max] : 999_999_999
+    @age_min = params[:age_min].blank? ? 0 : params[:age_min]
+    @age_max = params[:age_max].blank? ? 99 : params[:age_max]
+    @value_min = params[:value_min].blank? ? 0 : params[:value_min]
+    @value_max = params[:value_max].blank? ? 999_999_999 : params[:value_max]
     @position = params[:position]
-    @url = "#{@base_url}/similarities?player_id=#{sofifa_id}&age_min=#{age_min}age_max=#{age_max}&value_min=#{value_min}&value_max=#{value_max}&position=#{position}"
+    @url = "#{@base_url}/similarities?player_id=#{sofifa_id}&age_min=#{age_min}&age_max=#{age_max}&value_min=#{value_min}&value_max=#{value_max}&position=#{position}"
   end
 
   def call
     response = HTTParty.get(url).body
     players = JSON.parse(response)
-    players.map do |player|
-      player = Player.find_by(sofifa_id: player['sofifa_id'])
-      player.match = player['score']
+    players.map do |hash|
+      player = Player.find_by(sofifa_id: hash['sofifa_id'])
+      player.match = hash['score']
+      player
     end
   end
 end
